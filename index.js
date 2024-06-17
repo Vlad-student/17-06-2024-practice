40a4bdbccc441c04d05e8720bf109b7cfc5d0f82
Vlad-student <vlad.v.terletskiy@gmail.com>
2024-06-17 18:40:47 +0300

    function practice fgh

--- index.js
+++ index.js
@@ -42,9 +42,30 @@ console.log(numberMultiple(4,2));
 console.groupEnd();
 
 console.group('4 task')
+/**
+ * 
+ * @param {number} number1 
+ * @param {number} number2 
+ * @returns {number}
+ */
+function calculateareaTriangle(number1,number2) {
+    return number1*number2/2;
+}
+console.log(calculateareaTriangle(7.5,8));
+console.groupEnd();
 
-
-
-
+console.group('5 task')
+/**
+ * 
+ * @param {number} cost 
+ * @param {number'} percent default =20
+ * @returns {number}
+ */
+function costProductPlusPercent(cost,percent) {
+    let PDV = cost*percent/100;
+    let totalCost = cost+PDV;
+    return totalCost
+}
+console.log(costProductPlusPercent(12,10));
 console.groupEnd();
 
